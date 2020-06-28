997d2926b8f4b647362bcac13072e09a9492a942
kevin <kevin@betacode.io>
2020-06-28 21:55:54 +0800

    setup fetch listings query

--- src/sections/Listings/Listings.tsx
+++ src/sections/Listings/Listings.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
+import { server } from '../../lib/api';
 
+const LISTINGS = `
+  query Listings{
+    listings {
+      id
+      title
+      image
+      address
+      price
+      numOfGuests
+      numOfBeds
+      numOfBaths
+      rating
+    }
+  }
+`;
 interface Props {
   title: string;
 }
 
 const Listings = ({ title }: Props) => {
-  return <h2>{title}</h2>;
+  const fetchListings = async () => {
+    const response = await server.fetch({ query: LISTINGS });
+    console.log('fetch listings!', response.data);
+  };
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      <button onClick={fetchListings}>Query listing</button>
+    </div>
+  );
 };
 
 export { Listings };
