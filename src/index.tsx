e9a42a165e1d2d37749b1de0f8f635d6f40401b8
kevin <kevin@betacode.io>
2020-07-08 09:29:52 +0800

    reun login mutation (from cookie) when app is first render. add loading skeleton

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
-import { Affix, Layout } from 'antd';
+import { ApolloProvider, useMutation } from 'react-apollo';
+import { Affix, Layout, Spin } from 'antd';
 import { AppHeader, Home, Host, Listing, Listings, NotFound, User, Login } from './sections';
 import { Viewer } from './lib/types';
 import * as serviceWorker from './serviceWorker';
 import './styles/index.css';
+import { LOGIN } from './lib/graphql/mutations/';
+import {
+  Login as LoginData,
+  LoginVariables,
+} from './lib/graphql/mutations/Login/__generated__/Login';
+import { AppHeaderSkeleton, ErrorBanner } from './lib/components';
 
 const client = new ApolloClient({
   uri: '/api',
@@ -23,10 +29,39 @@ const initialViewer: Viewer = {
 
 const App = () => {
   const [viewer, setViewer] = useState<Viewer>(initialViewer);
+  const [login, { error }] = useMutation<LoginData, LoginVariables>(LOGIN, {
+    onCompleted: data => {
+      if (data && data.login) {
+        setViewer(data.login);
+      }
+    },
+  });
+
+  const loginRef = useRef(login);
+
+  useEffect(() => {
+    loginRef.current();
+  }, []);
+
+  if (!viewer.didRequest && !error) {
+    return (
+      <Layout className="app-skeleton">
+        <AppHeaderSkeleton />
+        <div className="app-skeleton__spin-section">
+          <Spin size="large" tip="Launching TinyHouse" />
+        </div>
+      </Layout>
+    );
+  }
+
+  const loginErrorBanner = error ? (
+    <ErrorBanner description="Login failed. Please try again later" />
+  ) : null;
 
   return (
     <Router>
       <Layout id="app">
+        {loginErrorBanner}
         <Affix offsetTop={0} className="app__affix-header">
           <AppHeader viewer={viewer} setViewer={setViewer} />
         </Affix>
